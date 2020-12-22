32d5774aa8cb65f45064068c5131b1b70582f7fd
JoseIgnacioTapia <josignaciot@gmail.com>
2020-12-22 17:42:44 -0300

    Display Balance Income  Expense

--- script.js
+++ script.js
@@ -17,7 +17,7 @@ let transactions = dummyTransactions;
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
   // Get sign
-  const sign = document.amount < 0 ? '-' : '+';
+  const sign = transaction.amount < 0 ? '-' : '+';
 
   const item = document.createElement('li');
 
@@ -32,11 +32,32 @@ function addTransactionDOM(transaction) {
   list.appendChild(item);
 }
 
+// Update the balance, income and expense
+function updateValues() {
+  const amounts = transactions.map(transaction => transaction.amount);
+
+  const total = amounts.reduce((acc, item) => (acc += item), 0).toFixed(2);
+
+  const income = amounts
+    .filter(item => item > 0)
+    .reduce((acc, item) => (acc += item), 0)
+    .toFixed(2);
+
+  const expense = (
+    amounts.filter(item => item < 0).reduce((acc, item) => (acc += item), 0) * -1
+  ).toFixed(2);
+
+  balance.innerText = `$${total}`;
+  moneyPlus.innerText = `$${income}`;
+  moneyMinus.innerText = `$${expense}`;
+}
+
 // Init app
 function init() {
   list.innerHTML = '';
 
   transactions.forEach(addTransactionDOM);
+  updateValues();
 }
 
 init();
