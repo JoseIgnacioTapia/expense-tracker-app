25ddc4f2a92281dc079b63e84bd76c3adb40d2f5
JoseIgnacioTapia <josignaciot@gmail.com>
2020-12-22 18:44:33 -0300

    Add & Delete Transactions

--- script.js
+++ script.js
@@ -3,6 +3,7 @@ const moneyPlus = document.getElementById('money-plus');
 const moneyMinus = document.getElementById('money-minus');
 const list = document.getElementById('list');
 const form = document.getElementById('form');
+const text = document.getElementById('text');
 const amount = document.getElementById('amount');
 
 const dummyTransactions = [
@@ -13,6 +14,35 @@ const dummyTransactions = [
 ];
 
 let transactions = dummyTransactions;
+
+// Add transaction
+function addTransaction(e) {
+  e.preventDefault();
+
+  if (text.value.trim() === '' || amount.value.trim() === '') {
+    alert('Please add a text and amount');
+  } else {
+    const transaction = {
+      id: generateID(),
+      text: text.value,
+      amount: +amount.value
+    };
+
+    transactions.push(transaction);
+
+    addTransactionDOM(transaction);
+
+    updateValues();
+
+    text.value = '';
+    amount.value = '';
+  }
+}
+
+// Generate random ID
+function generateID() {
+  return Math.floor(Math.random() * 100000000);
+}
 
 // Add transactions to DOM list
 function addTransactionDOM(transaction) {
@@ -26,7 +56,7 @@ function addTransactionDOM(transaction) {
 
   item.innerHTML = `
     ${transaction.text} <span>${sign}${Math.abs(transaction.amount)}
-    </span> <button class="delete-btn">x</button>
+    </span> <button class="delete-btn" onclick="removeTransaction(${transaction.id})">x</button>
   `;
 
   list.appendChild(item);
@@ -52,6 +82,13 @@ function updateValues() {
   moneyMinus.innerText = `$${expense}`;
 }
 
+// Remove transaction by ID
+function removeTransaction(id) {
+  transactions = transactions.filter(transaction => transaction.id !== id);
+
+  init();
+}
+
 // Init app
 function init() {
   list.innerHTML = '';
@@ -61,3 +98,5 @@ function init() {
 }
 
 init();
+
+form.addEventListener('submit', addTransaction);
